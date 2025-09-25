7455cc3e60b8112fef3c9e638cc4d3430bcebd95
RBC <raqblacor2@alu.edu.gva.es>
2025-09-25 19:07:49 +0200

    creado el esqueleto de la funcion calcularTotalGastos y añadida a export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -57,6 +57,10 @@ function borrarGasto(){
 
 }
 
+function calcularTotalGastos(){
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -66,5 +70,6 @@ export   {
     CrearGasto, 
     listarGastos,
     anyadirGasto,
-    borrarGasto     
+    borrarGasto,
+    calcularTotalGastos     
 }
