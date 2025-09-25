18253a1db48e3debb7e5a225a702eda585ae8c0a
RBC <raqblacor2@alu.edu.gva.es>
2025-09-25 19:09:10 +0200

    creado el esqueleto de la funcion calcularBalance y añadida a export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -61,6 +61,9 @@ function calcularTotalGastos(){
 
 }
 
+function calcularBalance(){
+
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -71,5 +74,6 @@ export   {
     listarGastos,
     anyadirGasto,
     borrarGasto,
-    calcularTotalGastos     
+    calcularTotalGastos,
+    calcularBalance     
 }
