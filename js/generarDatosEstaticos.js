3da5322b7553318c89e7618a5a3619213072ed3d
dkjeanr <raqblacor2@alu.edu.gva.es>
2025-10-29 12:45:22 +0100

    Carga generar

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,4 +1,9 @@
-import * as gP from "./gestionPresupuesto"
-import * as gPW from "./gestionPresupuestoWeb"
+import * as gP from "./gestionPresupuesto.js"
+import * as gPW from "./gestionPresupuestoWeb.js"
+
+gp.actualizarPresupuesto(1500);
+gpWeb.mostrarDatoEnId('presupuesto', gp.mostrarPresupuesto());
+
+// let g1 = new gP.CrearGasto(descripcion,valor, fecha, ...etiquetas);
 
 
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,5 +1,9 @@
+"use strict";
 function mostrarDatoEnId (idElemento,valor){
-
+    let element = document.getElementById('idElemento');
+    if(element){
+        element.textContent = String(valor);
+    }
 }
 function mostrarGastoWeb (){
     
