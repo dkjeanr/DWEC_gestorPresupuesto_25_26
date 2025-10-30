d186938c4d118fb0a1ac8eaf6c84dd6132271aef
dkjeanr <raqblacor2@alu.edu.gva.es>
2025-10-30 12:16:54 +0100

    mostrarGastosAgrupadosWeb sin test

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,4 +1,7 @@
 "use strict";
+
+import { createElement } from "react";
+
 function mostrarDatoEnId (idElemento,valor){
     let element = document.getElementById(idElemento);
     if(element){
@@ -10,11 +13,14 @@ function mostrarDatoEnId (idElemento,valor){
 }
 function mostrarGastoWeb (idElemento, gasto){
     let element = document.getElementById(idElemento);
-    if(element){
+    if (!element) {
+        console.log(`Error: No existe el elemento con id ${idElemento}`);
+        return;
+    }
         //Creo divPadre y le asigno una clase
     let divGasto = document.createElement('div');
     divGasto.classList.add('gasto');
-    element.appendChild(divGasto);
+    // element.appendChild(divGasto);
 
     //Creo divHijo, le asigno una clase, le asigno texto y lo añado al divPadre
     let divDescripcion = document.createElement('div');
@@ -31,29 +37,57 @@ function mostrarGastoWeb (idElemento, gasto){
     divValor.classList.add('gasto-valor');
     divValor.textContent = `${gasto.valor} €`;
     divGasto.appendChild (divValor);
-
-    //Como genero las distintas etiquetas automáticamente?
     
     let divEtiquetas = document.createElement('div');
     divEtiquetas.classList.add('gasto-etiquetas');
-    // divEtiquetas.textContent = 'ETIQUETAS DEL GASTO';
     divGasto.appendChild (divEtiquetas);
-    for (let i = 0; i < gasto.etiquetas.length ; i++){
-        let spanEtiqueta = document.createElement ('span');
-        spanEtiqueta.classList.add('gasto-etiqueta');
-        spanEtiqueta.textContent = gasto.etiquetas[i];
-        divEtiquetas.appendChild (spanEtiqueta);
+    if (gasto.etiquetas && gasto.etiquetas.length > 0) {
+        for (let i = 0; i < gasto.etiquetas.length ; i++){
+            let spanEtiqueta = document.createElement ('span');
+            spanEtiqueta.classList.add('gasto-etiqueta');
+            spanEtiqueta.textContent = gasto.etiquetas[i];
+            divEtiquetas.appendChild (spanEtiqueta);
+            }
     }
-
     
 }
     
+//clasName o classList?
 
+function mostrarGastosAgrupadosWeb (idElemento,agrup,periodo){
+    let element = document.getElementById(idElemento);
+    if(element && agrup){
+        let divAgrupacion = document.createElement("div");
+        divAgrupacion.classList.add("agrupacion");
+        element.appendChild(divAgrupacion);
 
-}
-function mostrarGastosAgrupadosWeb (){
-    
-}
+        let h1Periodo = document.createElement("h1");
+        h1Periodo.textContent = `Gastos agrupados por ${periodo}`;// textpPerodo
+        divAgrupacion.appendChild(h1Periodo);
+
+        let claves = Object.keys(agrup);
+        claves.sort();
+        for(let i = 0; i < claves.length;i++){
+            let clave = claves[i];
+            let valor = agrup[clave];
+
+            let divAgrupDato = document.createElement("div");
+            divAgrupDato.classList.add("agrupacion-dato");
+            divAgrupacion.appendChild(divAgrupDato);
+
+            let spanADClave = createElement("span")
+            spanADClave.classList.add("agrupacion-dato-clave");
+            spanADClave.textContent = clave;
+            divAgrupDato.appendChild(spanADClave);
+
+            let spanADValor = document.createElement("span");
+            spanADValor.classList.add("agrupacion-dato-valor");
+            spanADValor.textContent = String(valor);
+            divAgrupDato.appendChild(spanADValor);
+        }
+    }
+} 
+
 
 export{
     mostrarDatoEnId,
